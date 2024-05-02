d4ed3385e825104b773d1557d371c910f56298a6
liekeboo <160623259+liekeboo@users.noreply.github.com>
2024-05-02 13:22:42 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,53 +1,37 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0) {
         basic.clearScreen()
-        basic.showLeds(`
-            . . . . .
-            . # # # .
-            . # . # .
-            . # # # .
-            . . . . .
-            `)
+        motor.motorStopAll()
     }
     if (receivedNumber == 1) {
         basic.clearScreen()
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # . # . #
-            . . # . .
-            . . # . .
-            `)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+        motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed)
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CW, speed)
     }
     if (receivedNumber == 2) {
         basic.clearScreen()
-        basic.showLeds(`
-            . . # . .
-            . . # . .
-            # . # . #
-            . # # # .
-            . . # . .
-            `)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+        motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, speed)
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, speed)
     }
     if (receivedNumber == 3) {
         basic.clearScreen()
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+        motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed)
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, speed)
     }
     if (receivedNumber == 4) {
         basic.clearScreen()
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # # # # #
-            . . . # .
-            . . # . .
-            `)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+        motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, speed)
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CW, speed)
     }
 })
+let speed = 0
 radio.setGroup(1)
+speed = 300
