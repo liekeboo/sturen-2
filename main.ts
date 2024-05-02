93ae2ffab05d5d752c2c1d54f64a97a7a8f66283
liekeboo <160623259+liekeboo@users.noreply.github.com>
2024-05-02 21:27:46 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,35 +1,39 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 0) {
-        basic.clearScreen()
+    if (input.pinIsPressed(TouchPin.P0)) {
+        if (receivedNumber == 0) {
+            basic.clearScreen()
+            motor.motorStopAll()
+        }
+        if (receivedNumber == 1) {
+            basic.clearScreen()
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+            motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed)
+            motor.MotorRun(motor.Motors.M4, motor.Dir.CW, speed)
+        }
+        if (receivedNumber == 2) {
+            basic.clearScreen()
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+            motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, speed)
+            motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, speed)
+        }
+        if (receivedNumber == 3) {
+            basic.clearScreen()
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+            motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed)
+            motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, speed)
+        }
+        if (receivedNumber == 4) {
+            basic.clearScreen()
+            motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+            motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+            motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, speed)
+            motor.MotorRun(motor.Motors.M4, motor.Dir.CW, speed)
+        }
+    } else {
         motor.motorStopAll()
-    }
-    if (receivedNumber == 1) {
-        basic.clearScreen()
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
-        motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed)
-        motor.MotorRun(motor.Motors.M4, motor.Dir.CW, speed)
-    }
-    if (receivedNumber == 2) {
-        basic.clearScreen()
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
-        motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, speed)
-        motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, speed)
-    }
-    if (receivedNumber == 3) {
-        basic.clearScreen()
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
-        motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed)
-        motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, speed)
-    }
-    if (receivedNumber == 4) {
-        basic.clearScreen()
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
-        motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, speed)
-        motor.MotorRun(motor.Motors.M4, motor.Dir.CW, speed)
     }
 })
 let speed = 0
