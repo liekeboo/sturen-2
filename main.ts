02e79b150609d5387a5beee6de3e7b31428a3815
liekeboo <160623259+liekeboo@users.noreply.github.com>
2024-05-13 11:30:52 +0200

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -47,6 +47,10 @@ radio.onReceivedNumber(function (receivedNumber) {
             . # . . .
             . . # . .
             `)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, speed)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, speed)
+        motor.MotorRun(motor.Motors.M3, motor.Dir.CW, speed)
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CCW, speed)
     }
     if (receivedNumber == 5) {
         basic.clearScreen()
@@ -60,6 +64,34 @@ radio.onReceivedNumber(function (receivedNumber) {
             # # # # #
             . . . # .
             . . # . .
+            `)
+    }
+    if (receivedNumber == 6) {
+        basic.clearScreen()
+        motor.motorStop(motor.Motors.M1)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, speed)
+        motor.MotorRun(motor.Motors.M3, motor.Dir.CCW, speed)
+        motor.motorStop(motor.Motors.M4)
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . # . #
+            . . . # #
+            . . # # #
+            `)
+    }
+    if (receivedNumber == 7) {
+        basic.clearScreen()
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, speed)
+        motor.motorStop(motor.Motors.M2)
+        motor.motorStop(motor.Motors.M3)
+        motor.MotorRun(motor.Motors.M4, motor.Dir.CW, speed)
+        basic.showLeds(`
+            . . # # #
+            . . . # #
+            . . # . #
+            . # . . .
+            # . . . .
             `)
     }
 })
